e4d66e09f5893ef6cfb54a77c94827c7b9548dcb
Zachary Schulz <zjschulz@gmail.com>
2019-09-22 17:48:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,12 @@ def monopoly_with_third_tier
 	{
 	  :pieces => 4,
 	  :names => {},
-	  :rent_in_dollars => {}
+	  :rent_in_dollars => {
+	    :one_piece_owned => 25,
+	    :two_pieces_owned => 50,
+	    :three_pieces_owned => 100,
+	    :four_pieces_owned => 200
+	  }   
 	} 
 	}
 end
